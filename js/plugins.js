05583f3c39b2b375038edbd355d4fcf63cda1ed5
Nick Satinover <nsatinover.aws@gmail.com>
2021-01-04 01:05:39 -0500

    row wins

--- js/plugins.js
+++ js/plugins.js
@@ -2,15 +2,12 @@
 
 let currentTurn = 'p1';
 let totalMoves = 0;
-const $gameBoardRows = $('.gameRow');
 
-const $pOneTurnBtn = $('#pOneTurnBtn');
-$pOneTurnBtn.addClass('green');
-$pOneTurnBtn.html('Your Turn');
+// const $gameSpots = $('.board-btn').toArray();
+// console.log( $($gameSpots[3]).html() );
 
-const $pTwoTurnBtn = $('#pTwoTurnBtn');
-$pTwoTurnBtn.addClass('red');
-$pTwoTurnBtn.html('Waiting');
+
+const { $pOneTurnBtn, $pTwoTurnBtn } = startGame();
 
 $(".board-btn").on('click', (event) => {
     const thisSquare = event.target;
@@ -28,6 +25,15 @@ $(".board-btn").on('click', (event) => {
             currentTurn = 'p1';
         }
 
+        const $gameSpots = $('.board-btn').toArray();
+        const winner = isWinner($gameSpots);
+        if(winner == 'X'){
+            alert('Player X Wins');
+        }
+        if(winner == 'O'){
+            alert('Player O Wins');
+        }
+
         totalMoves++;
         if(totalMoves == 9){
             alert('Game Over');
@@ -37,9 +43,57 @@ $(".board-btn").on('click', (event) => {
 
 });
 
+function isWinner(gameBoard){
+
+    // gameBoard.forEach(gameSpace => {
+    //     console.log( $(gameSpace).html() )
+    // });
+
+    let j = 0;
+    let k = 3;
+
+    // check rows
+    for(let i = 0; i < 3; i++){
+
+        let countX = 0; 
+        let countO = 0;
+
+        for(; j < k; j++){
+            if( $(gameBoard[j]).html() == 'X') { countX++; }
+            if( $(gameBoard[j]).html() == 'O') { countO++; }
+        }
+        console.log('x: ' + countX);
+        console.log('o:' + countO);
+
+        if( countX == 3 ) {
+            return 'X'; 
+        } 
+        else if( countO == 3) {
+            return 'O';
+        }
+        else {
+            countX = 0;
+            countO = 0;
+            k += 3;
+        }
+
+    }
+
+}
+
+function startGame() {
+    const $pOneTurnBtn = $('#pOneTurnBtn');
+    $pOneTurnBtn.addClass('green');
+    $pOneTurnBtn.html('Your Turn');
+
+    const $pTwoTurnBtn = $('#pTwoTurnBtn');
+    $pTwoTurnBtn.addClass('red');
+    $pTwoTurnBtn.html('Waiting');
+    return { $pOneTurnBtn, $pTwoTurnBtn };
+}
 
 function OrangeMoved(thisSquare) {
-    $(thisSquare).html('O');
+    $(thisSquare).html('X');
     $($pOneTurnBtn).removeClass('green');
     $($pOneTurnBtn).addClass('red');
     $($pOneTurnBtn).html('Waiting');
@@ -50,7 +104,7 @@ function OrangeMoved(thisSquare) {
 }
 
 function BlueMoved(thisSquare) {
-    $(thisSquare).html('X');
+    $(thisSquare).html('O');
     $($pOneTurnBtn).removeClass('red');
     $($pOneTurnBtn).addClass('green');
     $($pOneTurnBtn).html('Your Turn');
